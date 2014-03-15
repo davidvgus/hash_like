0c23e12d9e6e545a586b922cd98260b5fd8f31af
David Gustafson <git@radicalskeptic.net>
2014-03-14 17:02:06 -0700

    adds bracket and keys methods and some tests.

--- lib/hash_like.rb
+++ lib/hash_like.rb
@@ -7,7 +7,6 @@ class HashLike
   def initialize( buckets: 10)
     @count = 0;
     @buckets = Array.new(buckets) { Array.new }
-
   end
 
   def empty?
@@ -18,21 +17,53 @@ class HashLike
     @buckets.count
   end
 
+  def keys
+    key_list = []
+    @buckets.each do |bucket|
+      bucket.each {|item| key_list << item.first }
+    end
+    key_list
+  end
+
+  def [](key)
+    if (bucket_item = key_exists key)
+      bucket_item[1]
+    else
+      nil
+    end
+  end
+
   def []=(key, value)
     return nil if key == nil
     digest = make_digest key
     bucket = get_bucket digest
-    @buckets[bucket] = [key, value]
 
+    if (bucket_item = key_exists key)
+      bucket_item[1] = value
+    else
+      @buckets[bucket] << [key, value]
+      @count += 1
+    end
   end
 
 private
+
+  def key_exists key
+    digest = make_digest key
+    bucket = get_bucket digest
+    @buckets[bucket].each do |bucket_item|
+      if bucket_item.first == key
+        return bucket_item
+      end
+    end
+    false
+  end
 
   def get_bucket digest
     digest % @buckets.count
   end
 
   def make_digest str
-    Digest::MD5.hexdigest(str)
+    Digest::MD5.hexdigest(str).to_i(16)
   end
 end
--- spec/hash_like_spec.rb
+++ spec/hash_like_spec.rb
@@ -19,8 +19,37 @@ describe HashLike do
       let(:test_hash) { HashLike.new }
 
       it "increments count by one." do
-        expect{test_hash["nines"] = 999}.to change{test_hash.count}.from(0).to(1)
+        expect{test_hash["nines"]= 999}.to change{test_hash.count}.from(0).to(1)
       end
+    end
+  end
+
+  describe "#[]" do
+    let(:test_hash) { HashLike.new }
+
+    it "retrieves the correct key" do
+      test_hash["one"] = 1
+      test_hash["two"] = 2
+      test_hash["three"] = "banana"
+      test_hash["two"] = []
+
+      test_hash["three"].should == "banana"
+    end
+  end
+
+  describe "#keys" do
+    context "After adding an item."
+    let(:test_hash) { HashLike.new }
+
+    it "has a key for that item" do
+      test_hash["testing"] = 123
+      expect(test_hash.keys).to include("testing")
+    end
+
+    it "the count for keys matches the count for the object" do
+    test_hash["one"] = 1
+    test_hash["two"] = 2
+    test_hash.count.should eq(test_hash.keys.count)
     end
   end
 end
